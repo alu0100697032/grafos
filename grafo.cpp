e01dcbd8b691bec9c2daa983fba9c0fc586bbfdd
Victor Hernandez Perez <alu0100697032@ull.edu.es>
2015-03-19 11:40:51 +0000

    Structure changes

--- grafo.cpp
+++ grafo.cpp
@@ -1,4 +1,4 @@
- /*
+/*
  *  GRAFO.CPP - Implementación de la clase GRAFOS
  *
  *
@@ -8,77 +8,61 @@
 
 #include "grafo.h"
 
+//CONSTRUCTOR
+GRAFO::GRAFO(char nombrefichero[85], int &errorapertura) {
+	ElementoLista dummy;
+	ifstream textfile;
+	textfile.open(nombrefichero);
+	if (textfile.is_open()) {
+		unsigned i, j, k;
+		// leemos por conversion implicita el numero de nodos, arcos y el atributo dirigido
+		textfile >> (unsigned &) numero_nodos >> (unsigned &) numero_arcos
+				>> (unsigned &) dirigido;
+		// los nodos internamente se numeran desde 0 a n-1
+		// creamos las n listas de sucesores
+		LSucesores.resize(numero_nodos);
+		// leemos los m arcos y cramos las listas de sucesores LS
+		for (k = 0; k < numero_arcos; k++) {
 
+		}
+		textfile.close();
+		if (Es_dirigido() == 1) {
+			ListaPredecesores();
+		};
+		errorapertura = 0;
+	} else {
+		errorapertura = 1;
+	}
 
-GRAFO::~GRAFO()
-{
-...
+}
+//DESTRUCTOR
+GRAFO::~GRAFO() {
+
 }
 
-void GRAFO:: actualizar (char nombrefichero[85], int &errorapertura)
-{
-.....
+void GRAFO::actualizar(char nombrefichero[85], int &errorapertura) {
+
 }
 
-void GRAFO::Info_Grafo()
-{
-.....
+void GRAFO::Info_Grafo() {
+
 }
 
-unsigned GRAFO::Es_dirigido()
-{
+unsigned GRAFO::Es_dirigido() {
 	return dirigido;
 }
 
-void Mostrar_Lista(vector<LA_nodo> L)
-{
-...........
-};
+void Mostrar_Lista(vector<LA_nodo> L) {
 
-void GRAFO :: Mostrar_Listas (int l)
-{
- ..............       
-};
+}
+;
+
+void GRAFO::Mostrar_Listas(int l) {
+
+}
+;
 
 void GRAFO::ListaPredecesores() {
-.............
-}
-
-
-
-
-GRAFO::GRAFO(char nombrefichero[85], int &errorapertura)
-{
-	ElementoLista     dummy;
-	ifstream textfile;
-	textfile.open(nombrefichero);
-	if (textfile.is_open())
-    {
-		unsigned i, j, k;
-		// leemos por conversion implicita el numero de nodos, arcos y el atributo dirigido
-		textfile >> (unsigned &) n >> (unsigned &) m >> (unsigned &) dirigido;
-		// los nodos internamente se numeran desde 0 a n-1
-		// creamos las n listas de sucesores
-		LS.resize(n); 
-		// leemos los m arcos y cramos las listas de sucesores LS
-		for (k=0;k<m;k++)
-        {
-			...
-			...
-			...
-		}
-		textfile.close();
-		if (dirigido == 1)
-        {
-            ListaPredecesores();
-        };
-        errorapertura = 0;
-	}
-	else
-    {
-		errorapertura = 1;
-    }
 
 }
-
 
--- grafo.h
+++ grafo.h
@@ -8,7 +8,6 @@
 #ifndef _GRAPH_H
 #define _GRAPH_H
 
-
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,34 +21,30 @@ const int maxint = 1000000;
 
 // definicion de un elemento de la lista de adyacencia
 
-typedef struct
-{
+typedef struct {
 	unsigned j; // nodo
-	int      c; // atributo para expresar el peso, la longitud, el coste, la inversin, etc...
+	int c; // atributo para expresar el peso, la longitud, el coste, la inversin, etc...
 } ElementoLista;
 
 // definicion del tipo de una lista de adyacencia de un nodo
 
 typedef vector<ElementoLista> LA_nodo;
 
+class GRAFO {
 
-class GRAFO
- {
-	// unsigned            dirigido;  //0 si el grafo es no dirigido y 1 si es dirigido
-	unsigned            n;         // numero de NODOS //
-	unsigned            m;         // numero de ARCOS //
-	unsigned   			dirigido;  //0 si el grafo es no dirigido y 1 si es dirigido
-	vector<LA_nodo>     LS;        // Lista de adyacencia de los sucesores: es una matriz donde cada fila posiblemente es posiblemente de distinto tamaño//
-	vector<LA_nodo>     LP;        // Lista de adyacencia de los predecesores: es una matriz donde cada fila posiblemente es posiblemente de distinto tamaño//
- public:
-     GRAFO(char nombrefichero[], int &errorapertura);
-     void actualizar (char nombrefichero[], int &errorapertura);
-     unsigned Es_dirigido();  //devuelve 0 si el grafo es no dirigido y 1 si es dirigido
-     void Info_Grafo();
-     void Mostrar_Listas(int l);
-     void ListaPredecesores();
-     ~GRAFO();
+	unsigned numero_nodos;         // numero de NODOS //
+	unsigned numero_arcos;         // numero de ARCOS //
+	unsigned dirigido;  //0 si el grafo es no dirigido y 1 si es dirigido
+	vector<LA_nodo> LSucesores; // Lista de adyacencia de los sucesores: es una matriz donde cada fila posiblemente es posiblemente de distinto tamaño//
+	vector<LA_nodo> LPredecesores; // Lista de adyacencia de los predecesores: es una matriz donde cada fila posiblemente es posiblemente de distinto tamaño//
+public:
+	GRAFO(char nombrefichero[], int &errorapertura);
+	void actualizar(char nombrefichero[], int &errorapertura);
+	unsigned Es_dirigido(); //devuelve 0 si el grafo es no dirigido y 1 si es dirigido
+	void Info_Grafo();
+	void Mostrar_Listas(int l);
+	void ListaPredecesores();
+	~GRAFO();
 };
-
 
 #endif
